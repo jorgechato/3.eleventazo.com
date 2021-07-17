f88c0191b0859725b3dd5cff8f49be74616a84dd
jorgechato <hack@jorgechato.com>
2021-07-17 20:24:40 +0200

    UPDATE configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,47 +1,64 @@
 export default {
-  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
+    // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+    ssr: false,
 
-  // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+    // Target: https://go.nuxtjs.dev/config-target
+    target: 'static',
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'El Eventazo',
-    htmlAttrs: {
-      lang: 'es'
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+        title: 'El Eventazo',
+        htmlAttrs: {
+            lang: 'es'
+        },
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: '' },
+            { name: 'format-detection', content: 'telephone=no' }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ]
     },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: [
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [
+    ],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [
+    ],
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [
+        'nuxt-animejs',
+        '@nuxtjs/firebase',
+    ],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+    firebase: {
+        config: {
+            apiKey: process.env.FIRE_API_KEY,
+            authDomain: '<authDomain>',
+            projectId: '<projectId>',
+            storageBucket: '<storageBucket>',
+            messagingSenderId: '<messagingSenderId>',
+            appId: '<appId>',
+            measurementId: '<measurementId>'
+        },
+        services: {
+            database: true,
+        },
+    },
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {
+    }
 }
